5a6ab9b985e1d3512dc39bba88dacdfbfa7bb926
HA | Student Researcher <haruka.ono.6q@stu.hosei.ac.jp>
2025-11-12 17:56:49 +0900

    Optimize calculation using bit length method
    Refactor to calculate answer using bit length.

--- ABC068/B.py
+++ ABC068/B.py
@@ -15,7 +15,7 @@ for i in range(1,N+1):
     
 print(index_)
 '''
-
+'''
 N=int(input())
 if N==1:
   print("1")
@@ -28,3 +28,17 @@ while calc_<=N/2:
   count_+=1
   
 print(calc_)
+'''
+
+N = int(input())
+
+# N.bit_length() はNを2進数で表すのに必要な桁数を返す
+# 例: N=7 (111) -> 3
+# 例: N=8 (1000) -> 4
+bit_len = N.bit_length()
+
+# 答えは 2**(ビット長 - 1)
+# 1 を (ビット長 - 1) 回左にシフトするのと同じ
+ans = 1 << (bit_len - 1)
+
+print(ans)
