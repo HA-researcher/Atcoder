161d13fd7eacdec67d042dc18727a628445fb37e
HA | Student Researcher <haruka.ono.6q@stu.hosei.ac.jp>
2025-11-12 17:43:13 +0900

    Update logic for handling input and calculations
    Refactor logic to handle N=1 case and improve calculation.

--- ABC068/B.py
+++ ABC068/B.py
@@ -1,7 +1,8 @@
+'''
 N=int(input())
 
 count_=0
-index_=0
+index_=1
 for i in range(1,N+1):
   count2=0
   index2=i
@@ -13,3 +14,17 @@ for i in range(1,N+1):
     index_=index2
     
 print(index_)
+'''
+
+N=int(input())
+if N==1:
+  print("1")
+  exit()
+
+count_=0
+calc_=1
+while calc_<=N/2:
+  calc_*=2
+  count_+=1
+  
+print(calc_)
