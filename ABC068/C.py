b8664d3fb0ddbf7f355ae4444a93a4b3a43b886c
HA | Student Researcher <haruka.ono.6q@stu.hosei.ac.jp>
2025-11-13 15:36:32 +0900

    Implement possibility check for connections

--- ABC068/C.py
+++ ABC068/C.py
@@ -1,3 +1,28 @@
+N,M=list(map(int,input().split()))
+A=[0]*(M+1)
+B=[0]*(M+1)
+for i in range(M):
+  A[i],B[i]=list(map(int,input().split()))
+
+search=set()
+search2=set()
+for i in range(M):
+  if B[i]==N:
+    search.add(A[i])
+  if A[i]==1:
+    search2.add(B[i])
+
+for i in search:
+  if i in search2:
+    print("POSSIBLE")
+    exit()
+
+print("IMPOSSIBLE")
+
+
+
+
+"""
 #TLEする
 
 N,M=list(map(int,input().split()))
@@ -16,3 +41,4 @@ for i in range(M):
         exit()
 
 print("IMPOSSIBLE")
+"""
