c2cdf48558a0f3081ffb864b5ee100ab58f21f2c
HA | Student Researcher <haruka.ono.6q@stu.hosei.ac.jp>
2025-12-09 10:21:54 +0900

    Correct N calculation in B.py
    Fix the calculation of N to correctly compute the number of operations needed based on A and B.

--- ABC139/B.py
+++ ABC139/B.py
@@ -1,12 +1,14 @@
 #+(A-1)*N
+"""
 import math
-
+"""
 A,B=map(int,input().split())
 """
 N=0
 while B>1+(A-1)*N:
     N+=1
-    """
 N=math.ceil((B-1)/(A-1))
-
+"""
+#X/Yの切り上げ演算、(X+Y-1)//Y
+N=(B-1+A-1-1)//(A-1)
 print(N)
