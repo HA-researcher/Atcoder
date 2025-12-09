9f3cf4defd5042910ed09b518c65f3122cc01dbf
HA | Student Researcher <haruka.ono.6q@stu.hosei.ac.jp>
2025-12-09 10:03:15 +0900

    Refactor calculation of minimum sum of squares

--- ABC156/C.py
+++ ABC156/C.py
@@ -1,4 +1,14 @@
+N = int(input())
+X = list(map(int, input().split()))
 
+#数学的な解法。下に凸なので微分して=0の地点を探すと、sum(X)/Nとわかる。それを最近整数にする。
+avg = sum(X) / N
+P = round(avg)
+
+ans = sum((x - P)**2 for x in X)
+
+print(ans)
+"""
 N=int(input())
 X=list(map(int,input().split()))
 
@@ -14,9 +24,7 @@ for P in range(MINX,MAXX+1):
     for i in range(N):
         SUM+=(X[i]-P)**2
     MINHP=min(MINHP,SUM)
-    MINP=min(MINP,P)
-
-    
+    MINP=min(MINP,P) 
 print(MINHP)
-
+"""
 
