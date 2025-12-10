b319d3ac86b7261aebd2d9d85739ec74de134b26
HA | Student Researcher <haruka.ono.6q@stu.hosei.ac.jp>
2025-12-10 16:44:25 +0900

    Refactor string grouping and output logic
    Refactor code to consolidate string processing and output.

--- ABC019/B.py
+++ ABC019/B.py
@@ -1,5 +1,15 @@
 import itertools
 
+S = input()
+ans = []
+
+for key, group in itertools.groupby(S):
+    # 文字列を作ってリストに追加
+    ans.append(f"{key}{len(list(group))}")
+print("".join(ans))
+"""
+import itertools
+
 S=input()
 KeyList=[]
 
@@ -7,3 +17,4 @@ for key,group in itertools.groupby(S):
     print(key,end="")
     print(len(list(group)),end="")
 print("\n",end="")
+"""
