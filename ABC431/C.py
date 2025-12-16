6c389f3de1a900a6127b21ffa4cef09bd29f4f4c
HA | Student Researcher <haruka.ono.6q@stu.hosei.ac.jp>
2025-12-16 10:25:24 +0900

    Implement comparison logic for lists H and B
    Added logic to compare elements of lists H and B with indices.

--- ABC431/C.py
+++ ABC431/C.py
@@ -6,6 +6,17 @@ H.sort(reverse=True)
 B.sort(reverse=True)
 
 Check=0
+
+ind_N=0
+ind_M=0
+while ind_N<N and ind_M<M:
+    if H[ind_N]<=B[ind_M]:
+        Check+=1
+        ind_N+=1
+        ind_M+=1
+    else:
+        ind_N+=1
+"""
 j=0
 i=0
 while i<M and i+j<N:
@@ -14,6 +25,7 @@ while i<M and i+j<N:
         i+=1
     else:
         j+=1
+"""
 if Check>=K:
     print("Yes")
 else:
