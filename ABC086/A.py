aa7ae702803b402700b721fd7450dcc067ea8a4f
HA | Student Researcher <haruka.ono.6q@stu.hosei.ac.jp>
2026-01-31 16:19:50 +0900

    Refactor to use OddOrEven function for parity check

--- ABC086/A.py
+++ ABC086/A.py
@@ -1,5 +1,20 @@
+#関数化
+def OddOrEven(a,b):
+    if a%2==0 or b%2==0:
+        return "Even"
+    else:
+        return "Odd"
+
+def solve():
+    a,b=map(int,input().split())
+    print(OddOrEven(a,b))
+if __name__ == "__main__":
+    solve()
+    
+"""
 a, b = map(int, input().split())
 if a * b % 2:
     print("Odd")
 else:
     print("Even")
+"""
