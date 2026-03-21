a2b31eff7f1bc81e0f6068c45ba15fd0802d0cc1
HA | Student Researcher <221439412+HA-researcher@users.noreply.github.com>
2026-03-22 00:53:07 +0900

    Implement BFS for maze traversal

--- ABC007/C.py
+++ ABC007/C.py
@@ -1,3 +1,32 @@
+from collections import deque
+
+def BFS(Row,Col,start_y,start_x,goal_y,goal_x):
+    start_y-=1; start_x-=1; goal_y-=1; goal_x-=1
+    dy=[0,0,1,-1]
+    dx=[1,-1,0,0]
+    maze=[input() for _ in range(Row)]
+    queue=deque([])
+    visited=set()
+    visited.add((start_y,start_x))
+    distance=[[-1]*Col for _ in range(Row)]; distance[start_y][start_x]=0
+    queue.append([start_y,start_x])
+    while queue:
+        y,x=queue.popleft()
+        for i in range(4):
+            ny=y+dy[i]; nx=x+dx[i]
+            if ((ny,nx) not in visited) and (0<=ny<Row) and (0<=nx<Col) and (maze[ny][nx]=="."):
+                visited.add((ny,nx))
+                queue.append([ny,nx])
+                distance[ny][nx]=distance[y][x]+1
+                if ny ==goal_y and nx == goal_x:
+                    return(distance[ny][nx])
+
+R,C=map(int,input().split())
+sy,sx=map(int,input().split())
+gy,gx=map(int,input().split())
+print(BFS(R,C,sy,sx,gy,gx))
+
+"""
 dy=[1,-1,0,0]
 dx=[0,0,1,-1]
 R,C=map(int,input().split())
@@ -28,7 +57,4 @@ while queue:
             visited.add((ny,nx))
             dist[ny][nx]=dist[y][x]+1
             queue.append((ny,nx))
-        
-    
-    
-    
+        """
